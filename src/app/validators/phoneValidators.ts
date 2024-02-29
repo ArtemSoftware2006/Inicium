1857b6cc6c9cd199442becdf1ab6696313bb4bcf
Artem <artem.egorov.art2006@gmail.com>
2024-02-29 15:37:36 +0300

    Добавил валидатор телефона

---
+++ src/app/validators/phoneValidators.ts
@@ -0,0 +1,8 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function russianPhoneNumberValidator(): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const valid = /^((\+7|7|8)+([0-9]){10})$/.test(control.value);
+    return valid ? null : { 'invalidPhone': { value: control.value } };
+  };
+}
