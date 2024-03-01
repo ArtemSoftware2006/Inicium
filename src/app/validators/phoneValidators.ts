4f32d55afffe38f08a06787e2370ce797b7fe37b
Artem <artem.egorov.art2006@gmail.com>
2024-03-01 15:04:44 +0300

    убрал consol.log

--- src/app/validators/phoneValidators.ts
+++ src/app/validators/phoneValidators.ts
@@ -3,10 +3,8 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 export function russianPhoneNumberValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     if (control.value == "") {
-      console.log("vali");
       return null;
     }
-    console.log(control.value);
     const valid = /^((\+7|7|8)+([0-9]){10})$/.test(control.value);
     return valid ? null : { 'invalidPhone': { value: control.value } };
   };
